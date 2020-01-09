109665cf9d139154f7016f0f610fb814445e32e9
lit-poks <pokhrel.lalit99@gmail.com>
2020-01-09 18:22:53 +0600

    Updates

---
+++ precache-manifest.d56038040dbb8fe1fb2eaad29a901bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffce170e70441f402494681c69e31980",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "35cce42f4fa8ad71e1cf",
+    "url": "/weather-app/static/css/main.10638008.chunk.css"
+  },
+  {
+    "revision": "c1c7f8e88029c52e1269",
+    "url": "/weather-app/static/js/2.11358144.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.11358144.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35cce42f4fa8ad71e1cf",
+    "url": "/weather-app/static/js/main.e5e9bce2.chunk.js"
+  },
+  {
+    "revision": "392f57512e27acace9e1",
+    "url": "/weather-app/static/js/runtime-main.1044bf1f.js"
+  },
+  {
+    "revision": "ab90635dee9909b4002cdfba390ac410",
+    "url": "/weather-app/static/media/watersolid.ab90635d.svg"
+  }
+]);
